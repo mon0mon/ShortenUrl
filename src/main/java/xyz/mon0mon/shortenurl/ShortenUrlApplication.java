41efe2a3663a3943c7414af66607a6e36fc8f069
이민기 <mon0mon@naver.com>
2024-10-05 08:28:06 +0900

    🎉 init : 프로젝트 신규 시작

---
+++ src/main/java/xyz/mon0mon/shortenurl/ShortenUrlApplication.java
@@ -0,0 +1,13 @@
+package xyz.mon0mon.shortenurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenUrlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShortenUrlApplication.class, args);
+  }
+
+}
---
+++ src/test/java/xyz/mon0mon/shortenurl/ShortenUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.mon0mon.shortenurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenUrlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
