d73188329822d7b725a1e352c5002e43df7b50e8
이민기 <mon0mon@naver.com>
2024-10-06 23:10:20 +0900

    ✨ feat : UUIDBase64 - UUIDBase64 디코딩 시, UUID 형식에 맞게 문자열이 출력되도록 수정

--- src/main/java/xyz/mon0mon/shortenurl/sample/UUIDBase64Example.java
+++ src/main/java/xyz/mon0mon/shortenurl/sample/UUIDBase64Example.java
@@ -14,6 +14,8 @@ public class UUIDBase64Example {
     // Base64로 인코딩
     String base64UUID = Base64.getUrlEncoder().withoutPadding().encodeToString(uuidBytes);
     System.out.println("Base64 Encoded UUID: " + base64UUID + " (" + base64UUID.length() + ")");
+    String decodedBase64UUID = convertBase64ToString(Base64.getUrlDecoder().decode(base64UUID));
+    System.out.println("decodedBase64UUID : " + decodedBase64UUID + " (" + decodedBase64UUID.length() + ")");
   }
 
   // UUID를 바이트 배열로 변환하는 메소드
@@ -27,5 +29,19 @@ public class UUIDBase64Example {
       bytes[8 + i] = (byte) (leastSigBits >>> 8 * (7 - i));
     }
     return bytes;
+  }
+
+  //  BASE64 Decoding 된 Byte 배열을 다시 String으로 Decode
+  public static String convertBase64ToString(byte[] hashBytes) {
+    StringBuilder sb = new StringBuilder();
+    for (byte hashByte : hashBytes) {
+      String hex = Integer.toHexString(0xFF & hashByte);
+      if (hex.length() == 1) {
+        sb.append('0');
+      }
+      sb.append(hex);
+    }
+    String unformattedString = sb.toString();
+    return String.format("%s-%s-%s-%s-%s", unformattedString.substring(0,8), unformattedString.substring(8,12), unformattedString.substring(12,16), unformattedString.substring(16,20), unformattedString.substring(20, 32));
   }
 }
